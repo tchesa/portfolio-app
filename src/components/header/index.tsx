554c3c8e29d8299f5849cebcfc952c8c9a1ee34d
tchesa <cesarolimpio@gmail.com>
2023-04-10 17:18:05 -0300

    remove template code

---
+++ src/components/header/index.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+
+const navItems = [
+  {
+    label: 'Resume',
+    href: '/'
+  },
+  {
+    label: 'Contact',
+    href: '/contact'
+  }
+]
+
+const Header = () => {
+  return (
+    <header className="flex justify-between py-4 px-6">
+      <Link className="font-medium" href="/">Cesar Lima</Link>
+      <nav>
+        <ul className="flex space-x-6">
+          {navItems.map(item => (
+            <li key={item.label}>
+              <Link className="hover:underline" href={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
+
