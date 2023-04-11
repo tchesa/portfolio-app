8d4c6a68d26d73e73e226e2159cf671877fd6022
tchesa <cesarolimpio@gmail.com>
2023-04-11 11:32:03 -0300

    fetch data from contentful

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,3 +1,6 @@
+import { Education, Employment } from "@/types/contentful"
+import { createClient } from "contentful"
+
 const classes = {
   sectionTitle: 'text-2xl pt-6',
   sectionList: 'flex flex-row px-6 pb-6 w-full',
@@ -5,7 +8,21 @@ const classes = {
   sectionListContent: 'grow pt-6',
 }
 
-const ResumePage = () => {
+const ResumePage = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || '',
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY || '',
+  })
+
+  const [employmentEntries, educationEntries] = await Promise.all([
+    client.getEntries<Employment>({
+      content_type: 'employment'
+    }),
+    client.getEntries<Education>({
+      content_type: 'education'
+    }),
+  ])
+
   return (
     <main className="py-4 container mx-auto">
       <div className="border"> {/* resume frame */}
@@ -17,27 +34,43 @@ const ResumePage = () => {
           <h2 id="employment" className={[classes.sectionTitle, classes.sectionListTitle].join(' ')}>Employment</h2>
           <div className={classes.sectionListContent}>
             <ul className="space-y-5">
-              <li>
-                <div>
-                  <div className="flex -ml-16">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn3rGXj6deSjZFRzgUqTYQiH6dt4oBDAew9w&usqp=CAU" className="w-[48px] h-[48px] object-cover mr-4" />
-                    <div className="grow">
-                      <div className="flex items-center">
-                        <h3 className="grow text-lg">Frontend Developer</h3>
-                        <span className="block text-sm text-neutral-400">2022 - 2023</span>
+              {employmentEntries.items.map(employment => (
+                <li key={employment.sys.id}>
+                  <div>
+                    <div className="flex -ml-16">
+                      {employment.fields.companyLogo && (
+                        <img src={employment.fields.companyLogo.fields.file.url} alt={`${employment.fields.companyName || 'Company'} logo`} className="w-[48px] h-[48px] object-cover mr-4" />
+                      )}
+                      <div className="grow">
+                        <div className="flex items-center">
+                          <h3 className="grow text-lg">{employment.fields.title || 'Untitled employment'}</h3>
+                          <span className="block text-sm text-neutral-400">
+                            {employment.fields.startDate ? employment.fields.startDate : null}
+                            {employment.fields.startDate && employment.fields.endDate ? ' - ' : null}
+                            {employment.fields.endDate ? employment.fields.endDate : null}
+                          </span>
+                        </div>
+                        <span className="block text-sm">
+                          {employment.fields.companyName ? employment.fields.companyName : null}
+                          {employment.fields.companyName && employment.fields.employmentType ? ' - ' : null}
+                          {employment.fields.employmentType ? employment.fields.employmentType : null}
+                        </span>
+                        <span className="block text-sm text-neutral-400">
+                          {employment.fields.companyLocation ? employment.fields.companyLocation : null}
+                          {employment.fields.companyLocation && employment.fields.locationType ? ' - ' : null}
+                          {employment.fields.locationType ? employment.fields.locationType : null}
+                        </span>
                       </div>
-                      <span className="block text-sm">AtomicVest Inc - Full-time</span>
-                      <span className="block text-sm text-neutral-400">United States - Remote</span>
                     </div>
+                    <p className="mt-1 mb-2 text-sm">
+                      {employment.fields.description}
+                    </p>
+                    {employment.fields.technologies && employment.fields.technologies?.length > 0 && (
+                      <span className="block text-sm text-neutral-400">Technologies: {employment.fields.technologies.join(', ')}</span>
+                    )}
                   </div>
-                  <p className="mt-1 mb-2 text-sm">
-                    - Maintained and improved an embedded app for investment and shares market, using React, TypeScript, and modern tools to convert Figma files into screens/components.
-                    - Created a Node.js SDK based on an OpenAPI schema JSON file. The library generates all the code by the schema file using Axios and typed methods. Developed a workflow that automatically updates the version every time the schema file gets updated.
-                    - Converted an old dashboard web app from Next.js to React using the most recent technologies.
-                  </p>
-                  <span className="block text-sm text-neutral-400">Technologies: Typescript, React, ...</span>
-                </div>
-              </li>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -45,32 +78,54 @@ const ResumePage = () => {
           <h2 id="education" className={[classes.sectionTitle, classes.sectionListTitle].join(' ')}>Education</h2>
           <div className={classes.sectionListContent}>
             <ul className="space-y-5">
-              <li>
-                <div>
-                  <div className="flex items-center">
-                    <h3 className="grow">Bachelor of Science, Computer Science</h3>
-                    <span className="block text-sm text-neutral-400">2015 - 1018</span>
+              {educationEntries.items.map(education => (
+                <li key={education.sys.id}>
+                  <div>
+                    <div className="flex items-center">
+                      <h3 className="grow">
+                        {education.fields.degree ? education.fields.degree : null}
+                        {education.fields.degree && education.fields.fieldOfStudy ? ' - ' : null}
+                        {education.fields.fieldOfStudy ? education.fields.fieldOfStudy : null}
+                      </h3>
+                      <span className="block text-sm text-neutral-400">
+                        {education.fields.startDate ? education.fields.startDate : null}
+                        {education.fields.startDate && education.fields.endDate ? ' - ' : null}
+                        {education.fields.endDate ? education.fields.endDate : null}
+                      </span>
+                    </div>
+                    <span className="block text-sm text-neutral-400">
+                      {education.fields.school ? education.fields.school : null}
+                      {education.fields.school && education.fields.schoolLocation ? ' - ' : null}
+                      {education.fields.schoolLocation ? education.fields.schoolLocation : null}
+                    </span>
+                    {education.fields.attachments && education.fields.attachments.length > 0 && (
+                      <>
+                        <h4 className="mt-2 mb-2">Attachments</h4>
+                        <ul>
+                          {education.fields.attachments.map(attachment => (
+                            <li key={attachment.sys.id}>
+                              <div className="flex">
+                                {/* {attachment.fields.thumbnail && (
+                                  <img src={attachment.fields.thumbnail.fields.file.url} alt={`${attachment.fields.title || 'Company'} logo`} className="w-[106px] h-[60px] border rounded object-cover mr-4" />
+                                )} */}
+                                <div>
+                                  {attachment.fields.title && <h5 className="text-sm">{attachment.fields.title}</h5>}
+                                  {attachment.fields.description && <span className="block text-sm text-neutral-400">{attachment.fields.description}</span>}
+                                </div>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </>
+                    )}
                   </div>
-                  <span className="block text-sm text-neutral-400">UFOP - Ouro Preto, Minas Gerais, Brazil</span>
-                  <h4 className="mt-2 mb-2">Attachments</h4>
-                  <ul>
-                    <li>
-                      <div className="flex">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn3rGXj6deSjZFRzgUqTYQiH6dt4oBDAew9w&usqp=CAU" className="w-[106px] h-[60px] border rounded object-cover mr-4" />
-                        <div>
-                          <h5 className="text-sm">Aperfeiçoamento automático dos conjuntos de termos utilizados pelo Yucca: um coletor temático baseado em gênero</h5>
-                          <span className="block text-sm text-neutral-400">Trabalho de monografia apresentado em 2018 na Universidade Federal de Ouro Preto.</span>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </li>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
       </div>
-    </main >
+    </main>
   )
 }
 
---
+++ src/types/contentful.ts
@@ -0,0 +1,56 @@
+import { Entry } from "contentful";
+
+export type EmploymentType =
+  | "fullTime"
+  | "partTime"
+  | "freelance"
+  | "selfEmployed"
+  | "internship"
+  | "contract";
+
+export type locationType = "onSite" | "hybrid" | "remote";
+
+export type ContentfulMedia = {
+  title: string;
+  description: string;
+  file: {
+    contentType: string;
+    details: {
+      image: { width: number; height: number };
+      size: number;
+    };
+    fileName: string;
+    url: string;
+  };
+};
+
+export type Employment = {
+  title?: string;
+  employmentType?: EmploymentType;
+  companyName?: string;
+  companyUrl?: string;
+  companyLogo?: Entry<ContentfulMedia>;
+  companyLocation?: string;
+  startDate?: string;
+  endDate?: string;
+  currentlyWorkingHere?: boolean;
+  description?: string;
+  technologies?: string[];
+  locationType?: locationType;
+};
+
+export type Attachment = {
+  title?: string;
+  description?: string;
+  link?: string;
+};
+
+export type Education = {
+  school?: string;
+  degree?: string;
+  fieldOfStudy?: string;
+  startDate?: string;
+  endDate?: string;
+  attachments?: Entry<Attachment>[];
+  schoolLocation?: string;
+};
