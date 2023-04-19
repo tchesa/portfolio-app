3162b82e262dc39ebb21e849dd90016caa6b7a66
tchesa <cesarolimpio@gmail.com>
2023-04-19 14:54:10 -0300

    remove navitems object

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,28 +2,18 @@ import Link from "next/link"
 import { Shape1 } from "../shapes"
 import { FiGithub, FiTwitter } from "react-icons/fi"
 
-const navItems = [
-  {
-    label: 'Resume',
-    href: '/resume'
-  },
-  {
-    label: 'Contact',
-    href: '/contact'
-  }
-]
-
 const Header = () => {
   return (
     <header className="flex justify-between py-4 relative container mx-auto px-8">
       <Link className="font-medium font-serif text-3xl text-white hover:underline" href="/">Cesar Antunes</Link>
       <nav>
         <ul className="flex space-x-6 items-center">
-          {navItems.map(item => (
-            <li key={item.label}>
-              <Link className="hover:underline text-lg font-medium font-serif text-orange-900" href={item.href}>{item.label}</Link>
-            </li>
-          ))}
+          <li>
+            <Link className="hover:underline text-lg font-medium font-serif text-orange-900" href="/resume">Resume</Link>
+          </li>
+          <li>
+            <Link className="hover:underline text-lg font-medium font-serif text-orange-900" href="/contact">Contact</Link>
+          </li>
           <li>
             <Link className="text-orange-900" href="https://github.com/tchesa" target="_blank">
               <FiGithub className="text-2xl" />
