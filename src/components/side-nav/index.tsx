6c7fc1234845812b8600630a85a8bf42f0736cc7
tchesa <cesarolimpio@gmail.com>
2023-04-21 12:47:02 -0300

    fix side nav

--- src/components/side-nav/index.tsx
+++ src/components/side-nav/index.tsx
@@ -26,7 +26,11 @@ const SideNav = ({ className }: Props) => {
         <FiMenu className="text-orange-900 text-2xl" />
       </button>
       <div className={`flex fixed w-screen h-screen top-0 right-0 z-10 overflow-hidden ${isOpen ? '' : 'pointer-events-none'}`}>
-        <button type="button" onClick={close} className={`backdrop-button block grow bg-black ${isOpen ? 'bg-opacity-40' : 'bg-opacity-0'}`} />
+        <button
+          type="button"
+          onClick={close}
+          className={`backdrop-button block grow bg-black ${isOpen ? 'bg-opacity-40' : 'bg-opacity-0'}`}
+        />
         <nav className={`side-panel block w-[300px] border-l-2 border-black bg-white ${isOpen ? 'mr-0' : '-mr-[300px]'}`}>
           <div className="text-right mb-6 pt-4 pr-5">
             <button type="button" className="p-2" onClick={close}>
@@ -35,10 +39,22 @@ const SideNav = ({ className }: Props) => {
           </div>
           <ul className="space-y-6 px-8">
             <li>
-              <Link className="hover:underline text-xl font-medium font-serif text-orange-900" href="/resume">Resume</Link>
+              <Link
+                className="hover:underline text-xl font-medium font-serif text-orange-900"
+                href="/resume"
+                onClick={close}
+              >
+                Resume
+              </Link>
             </li>
             <li>
-              <Link className="hover:underline text-xl font-medium font-serif text-orange-900" href="/contact">Contact</Link>
+              <Link
+                className="hover:underline text-xl font-medium font-serif text-orange-900"
+                href="/contact"
+                onClick={close}
+              >
+                Contact
+              </Link>
             </li>
           </ul>
         </nav>
