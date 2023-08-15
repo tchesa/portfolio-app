b7ec5ac4162705f029104b052f8b2c015eea7a4b
tchesa <cesarolimpio@gmail.com>
2023-08-15 11:19:32 -0300

    create test endpoint

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,10 +1,12 @@
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request) {
   const body = await request.json();
 
   console.log("test", body);
 
-  return {
+  return NextResponse.json({
     body,
     test: true,
-  };
+  });
 }
